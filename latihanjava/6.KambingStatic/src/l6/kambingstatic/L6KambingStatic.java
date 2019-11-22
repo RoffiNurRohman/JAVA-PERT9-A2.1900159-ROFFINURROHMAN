814568897646a2cf25eb5af4815975adc92459fc
RoffiNurRohman <roffinurrohman22@gmail.com>
2019-11-22 20:11:20 +0700

    $ git commit -m

---
+++ latihanjava/6.KambingStatic/src/l6/kambingstatic/L6KambingStatic.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l6.kambingstatic;
+
+class Mamalia  {
+    // Variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing;
+
+}
+
+
+/**
+ * NAMA  : Roffi Nur Rohman
+ * NIM   : A2.1900159
+ * Kelas : TI-1C
+ * 
+ * @author roffinurrohman
+ */
+public class L6KambingStatic {
+    //NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = "UDIN";
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + Mamalia.jumlahKambing);
+    }
+    
+}
